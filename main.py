7f21839e1b47577017905b16e2965f89c8388237
emilyho03 <71272619+emilyho03@users.noreply.github.com>
2020-12-09 16:34:24 +0800

    test

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 #screenshot function
-from kivy.app import app
-kivy.require("3.6.5")
-from kivy.uix.label import Label
-import PyAutoGUI
-    im1 = pyautogui.screenshot()
-    im1.save('my_screenshot.png')
-    im2 = pyautogui.screenshot('my_screenshot2.png')
-    button7location = pyautogui.locateOnScreen('button.png') # returns (left, top, width, height) of matching region
-    button7location
-    (1416, 562, 50, 41)
-    buttonx, buttony = pyautogui.center(button7location)
-    buttonx, buttony
-    (1441, 582)
-    pyautogui.click(buttonx, buttony)  # clicks the center of where the button was found
+from flask import flask, request
+
+app = Flask(__name__)
+
+@app.route('/home', methods=['GET', 'POST']) #rootdirectory, home page
+def home():
+    if request.method == 'POST':
+        return "You are using POST"
+    else:
+        return "You are probably using GET"
+
+@app.route('/profile/<username>')
+def profile(username):
+    return "<h1>Hey there %s! Here is your wallpaper<h1>" % username
+
+@app.route('/profile/<username>')
+def profile(username):
+    return "<h1>Hey there %s! Here is your wallpaper<h1>" % username
+
+
+if__name__=="__main__"
+    app.run(debug=True)
